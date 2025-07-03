d7250aff61e0e35cf77c9fa12d27f8fc5930d6bd
Jhoan David Sinisterra <sinisterravalenciajhoandavid@gmail.com>
2025-07-03 15:10:16 -0500

    Primer envio del proyecto

---
+++ src/test/java/com/ferreteriaSpringBoot/ferreteriaSpringBoot/FerreteriaSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ferreteriaSpringBoot.ferreteriaSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FerreteriaSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
