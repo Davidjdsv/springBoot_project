562de11716142f71834106b8f62c8dc941dd055f
Jhoan David Sinisterra <sinisterravalenciajhoandavid@gmail.com>
2025-07-03 18:46:21 -0500

    Avanzando en la vista

--- src/main/java/com/ferreteriaSpringBoot/ferreteriaSpringBoot/controller/VistaClientes.java
+++ src/main/java/com/ferreteriaSpringBoot/ferreteriaSpringBoot/controller/VistaClientes.java
@@ -1,5 +1,22 @@
 package com.ferreteriaSpringBoot.ferreteriaSpringBoot.controller;
 
+import com.ferreteriaSpringBoot.ferreteriaSpringBoot.repository.ClientesRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class VistaClientes {
+    @Autowired
+    ClientesRepository clientesRepository;
+
+    @GetMapping("vista/clientes")
+    public String getAll(Model model){
+        model.addAttribute("clientes", clientesRepository.findAll());
+        return "clientes";
+    }
+
+
 
 }
